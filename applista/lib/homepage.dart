6b91854a9d4ebcdb686e6c0a8aa72fee28550487
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2021-11-03 16:28:23 -0300

    interface start

--- applista/lib/homepage.dart
+++ applista/lib/homepage.dart
@@ -8,13 +8,60 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List _listaTarefas = ["Acordar", "Comer", "Academia", "Programar"];
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text("Lista De Tarefas"),
         backgroundColor: Colors.indigo,
       ),
+      body: Column(
+        children: [
+          Expanded(
+              child: ListView.builder(
+            itemCount: _listaTarefas.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(_listaTarefas[index]),
+              );
+            },
+          ))
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.indigo,
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text("Criar tarefa"),
+                  content: TextField(
+                    decoration: InputDecoration(labelText: "Digite sua tarefa"),
+                    onChanged: (text) {},
+                  ),
+                  actions: [
+                    TextButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: Text("Cancelar")),
+                    TextButton(
+                        onPressed: () {
+                          //salvar
+                          TextButton(onPressed: () {}, child: Text("Cancelar"));
+                        },
+                        child: Text("Salvar"))
+                  ],
+                );
+              });
+        },
+      ),
+      //bottomNavigationBar: BottomAppBar(),
     );
   }
 }
