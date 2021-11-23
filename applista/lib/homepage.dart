b4a18a906bf7c0b872f93f761652464c3df52a5e
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2021-11-23 16:08:23 -0300

    save data and change data

--- applista/lib/homepage.dart
+++ applista/lib/homepage.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,11 +13,57 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List _listaTarefas = ["Acordar", "Comer", "Academia", "Programar"];
+  List _listaTarefas = [];
+  TextEditingController _controllerTarefa = TextEditingController();
+  Future<File> _getFile() async {
+    final diretorio = await getApplicationDocumentsDirectory();
+    return File("${diretorio.path}/dados.json");
+  }
+
+  _salvarTarefa() {
+    String textoDigitado = _controllerTarefa.text;
+    //criar dados
+    Map<String, dynamic> tarefa = Map();
+    tarefa["titulo"] = textoDigitado;
+    tarefa["realizada"] = false;
+    setState(() {
+      _listaTarefas.add(tarefa);
+    });
+    _salvarArquivo();
+    _controllerTarefa.text = "";
+  }
+
+  _salvarArquivo() async {
+    var arquivo = await _getFile();
+
+    String dados = json.encode(_listaTarefas);
+    arquivo.writeAsString(dados);
+  }
+
+  _lerArquivo() async {
+    var arquivo = await _getFile();
+    try {
+      final arquivo = await _getFile();
+      return arquivo.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _lerArquivo().then((dados) {
+      setState(() {
+        _listaTarefas = json.decode(dados);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    var height = MediaQuery.of(context).size.width;
+    // var width = MediaQuery.of(context).size.width;
+    //var height = MediaQuery.of(context).size.width;
 
     return Scaffold(
       appBar: AppBar(
@@ -26,9 +77,18 @@ class _HomePageState extends State<HomePage> {
               child: ListView.builder(
             itemCount: _listaTarefas.length,
             itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(_listaTarefas[index]),
-              );
+              return CheckboxListTile(
+                  title: Text(_listaTarefas[index]["titulo"]),
+                  value: _listaTarefas[index]["realizada"],
+                  onChanged: (valorAlterado) {
+                    setState(() {
+                      _listaTarefas[index]["realizada"] = valorAlterado;
+                      _salvarArquivo();
+                    });
+                  });
+              //  return ListTile(
+              //
+              //  );
             },
           ))
         ],
@@ -43,6 +103,7 @@ class _HomePageState extends State<HomePage> {
                 return AlertDialog(
                   title: Text("Criar tarefa"),
                   content: TextField(
+                    controller: _controllerTarefa,
                     decoration: InputDecoration(labelText: "Digite sua tarefa"),
                     onChanged: (text) {},
                   ),
@@ -52,8 +113,9 @@ class _HomePageState extends State<HomePage> {
                         child: Text("Cancelar")),
                     TextButton(
                         onPressed: () {
+                          Navigator.pop(context);
+                          _salvarTarefa();
                           //salvar
-                          TextButton(onPressed: () {}, child: Text("Cancelar"));
                         },
                         child: Text("Salvar"))
                   ],
